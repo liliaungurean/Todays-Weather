0ff5855c6b2716e2e5bbfbb05303a350e05dfaf3
liliaungurean <lilicikau95@gmail.com>
2022-12-05 12:50:31 -0600

    Contiued JS

--- Develope/scrip.js
+++ Develope/scrip.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-var apiKey = "5ddd8035290f23820fc56c033adccb7c";        
+    
     
     $("#searchBtn").on("click", getSearchInput);
     $(document).on("click", ".selected", storedCities);
@@ -36,6 +36,31 @@ var apiKey = "5ddd8035290f23820fc56c033adccb7c";
         }
     }
     
+    var apiKey = "5ddd8035290f23820fc56c033adccb7c";    
+    function getWeather (city) {
 
-    getWeather("Minneapolis");
+        var queryURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&appid=" + apiKey;
+    
+        $.ajax({
+            url: queryURL,
+            method: "GET",
+            dataType: "jsonp",
+            success: function(response){
+                console.log(queryURL);
+                console.log(response);
+    
+                    function date_format(dt_string){
+                        var date = new Date(dt_string.dt*1000);
+                        return date.toDateString();
+                    }
+    
+                    function temp_trans(input){
+                        var temp =  "Temp: " + ((input.main.temp- 273.15) * 1.80 + 32).toFixed(2) + " F ";
+                        return temp;
+                    }
+            
+               $("#previousSearches").empty();
+    
+
+    getWeather("");
     });  
